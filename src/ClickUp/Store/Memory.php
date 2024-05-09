f7f4ddf0fb7cb24c163c7dcbaaa160be1d45ab4e
sreeraj <sreerajsrrk@gmail.com>
2024-05-09 16:30:24 +0530

    memory store update

--- src/ClickUp/Store/Memory.php
+++ src/ClickUp/Store/Memory.php
@@ -6,17 +6,23 @@ use ClickUp\Contracts\StateStorage;
 use ClickUp\Options;
 
 /**
- * Class Memory.
+ * Class Memory
+ *
+ * In-memory state storage implementation of StateStorage interface.
  */
 class Memory implements StateStorage
 {
     /**
-     * @var array
+     * In-memory container for storing data.
+     *
+     * @var array<string, array>
      */
-    protected $container = [];
+    protected array $container = [];
 
     /**
-     * @inheritDoc
+     * Retrieve all stored data.
+     *
+     * @return array<string, array>
      */
     public function all(): array
     {
@@ -24,7 +30,11 @@ class Memory implements StateStorage
     }
 
     /**
-     * @inheritDoc
+     * Retrieve data for a specific key based on options.
+     *
+     * @param Options $options Options containing the key for retrieval
+     *
+     * @return array The stored values associated with the given key
      */
     public function get(Options $options): array
     {
@@ -32,17 +42,27 @@ class Memory implements StateStorage
     }
 
     /**
-     * @inheritDoc
+     * Set data for a specific key.
+     *
+     * @param array $values Values to be stored
+     * @param Options $options Options containing the key for storage
+     *
+     * @return void
      */
-    public function set(array $values, Options $options)
+    public function set(array $values, Options $options): void
     {
         $this->container[$options->getStoreKey()] = $values;
     }
 
     /**
-     * @inheritDoc
+     * Push a new value to the front of the array for a specific key.
+     *
+     * @param mixed $value Value to be stored
+     * @param Options $options Options containing the key for storage
+     *
+     * @return void
      */
-    public function push($value, Options $options)
+    public function push(mixed $value, Options $options): void
     {
         $storeKey = $options->getStoreKey();
         if (!isset($this->container[$storeKey])) {
@@ -53,9 +73,13 @@ class Memory implements StateStorage
     }
 
     /**
-     * @inheritDoc
+     * Reset the stored values for a specific key.
+     *
+     * @param Options $options Options containing the key to reset
+     *
+     * @return void
      */
-    public function reset(Options $options)
+    public function reset(Options $options): void
     {
         $this->container[$options->getStoreKey()] = [];
     }
